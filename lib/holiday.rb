5a32f27446e64ca5b269dc2a90c7a88ac3db916e
Gladys Gutierrez <gutierrezgladys721@gmail.com>
2020-06-08 04:41:57 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -104,15 +104,20 @@ end
 
 
 def all_holidays_with_bbq(holiday_hash)
-  
-  
-  
-  
-  
+   bbq_array = []
+  holiday_hash.each do |season, holiday_supplies_hash|
+    holiday_supplies_hash.each do |holiday, supplies|
+      if supplies.include?("BBQ")
+        bbq_array << holiday
+      end
+    end
+  end
+  bbq_array
+end 
+
   # return an array of holiday names (as symbols) where supply lists
   # include the string "BBQ"
 
-end
 
 
 
